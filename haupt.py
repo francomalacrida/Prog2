46e18141917a5edf06fbfaa63a7d9704d4ae7afd
francomalacrida <61312551+francomalacrida@users.noreply.github.com>
2020-05-15 16:47:00 +0200

    Add files via upload

--- haupt.py
+++ haupt.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import jsonify
+from flask import url_for
 from datetime import datetime
 import json
 
 app = Flask("__name__")
 
-# Datei in Klammer anpassen, dass hier main.html geladen wird
+# Rendern der versch. Templates, für url_for
+@app.route('/')
 @app.route('/home')
-# @app.route('/home')
 def home():
    return render_template('main.html')
 
@@ -14,52 +18,65 @@ def home():
 def upload():
     return render_template('student.html')
 
+@app.route('/durchschnitt')
+def auswahl():
+    return render_template('auswahl.html')
+
+#write modus "w"
+def write_data_to_json(file_name, daten):
+  with open(file_name, "w") as open_file:
+    json.dump(daten,open_file)
+
+def read_data_from_json(file_name):
+  try: 
+    with open(file_name, "r") as open_file:
+      mein_eingelesenes_dict = json.load(open_file)
+      return mein_eingelesenes_dict
+  except Exception:
+    return []   
 
 #Hier soll das Resultat von result.html geladen werden
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
    if request.method == 'POST':
       result = request.form
-      
-      print(result['Name'])
-      print(result['Vorname'])
-      print(result['Fach'])
-      print(result['Note'])
+      print(result)
+      alle_noten = read_data_from_json("notenrechner.json")
+      name = result['Name']
+      vorname = result['Vorname']
+      semester = result['Semester']
+      fach = result['Fach']
+      note = result['Note']
+      neue_noten = {
+        "name": name,
+        "vorname": vorname,
+        "semester": semester,
+        "fach": fach,
+        "note": note,
+      }
 
-      #Speichern des Resultats als json-File
-      #def speichern(datei, key, value):
-      #  try:
-      #      with open(datei) as open_file:
-      #          datei_inhalt = json.load(open_file)
-      #      except FileNotFoundError:
-      #          datei_inhalt = {}
-
-      #      print(datei_inhalt)
-
-       #     with open(datei, "w") as open_file:
-       #         json.dump(datei_inhalt, open_file)
-
-      #def resultat_speichern(resultat):
-      #  datei_name = "resultat.json"
-      #  zeitpunkt = datetime.now()
-      #  speichern(date_name, zeitpunkt, resultat)
-      #  return zeitpunkt, resultat
-
-      #def resultat_laden():
-      #    datei_name = "resultat.json"
-
-      #    try: 
-      #      with open(datei_name) as open_file:
-      #          datei_inhalt = json.load(open_file)
-      #    except FileNotFoundError:
-      #      datei_inhalt = {}
-
-      #      return datei_inhalt
-
-
-
+      alle_noten.append(neue_noten)
+      write_data_to_json("notenrechner.json", alle_noten)
 
       return render_template("result.html",result = result)
+
+#Hier soll das Resultat von durchschnitt.html geladen werden
+@app.route('/durchschnitt',methods = ['POST', 'GET'])
+def durchschnitt():
+   if request.method == 'POST':
+      durchschnitt = request.form
+      print(durchschnitt)
+      #alle_noten = read_data_from_json("notenrechner.json")
+      name = durchschnitt['Name']
+      vorname = durchschnitt['Vorname']
+      semester = durchschnitt['Semester']
+      neue_auswahl = {
+        "name": name,
+        "vorname": vorname,
+        "semester": semester
+      }
+
+      return render_template("durchschnitt.html",result = neue_auswahl)   
 
 if __name__ == '__main__':
    app.run(debug = True, port=5000)
