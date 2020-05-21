03a358767d945db64d45465f6ee32fb454257248
francomalacrida <61312551+francomalacrida@users.noreply.github.com>
2020-05-21 17:17:58 +0200

    add Funktion schnittrechner für semester & studium

--- haupt.py
+++ haupt.py
@@ -8,84 +8,131 @@ import json
 
 app = Flask("__name__")
 
-# Rendern der versch. Templates, für url_for
+
+#Remdern des main.html Templates
 @app.route('/')
 @app.route('/home')
 def home():
-   return render_template('main.html')
+    return render_template('main.html')
 
-@app.route('/upload')
-def upload():
-    return render_template('student.html')
-
-@app.route('/auswahl')
-def auswahl():
-    return render_template('auswahl.html')
-
-#write modus "w"
+# write modus "w"
 def write_data_to_json(file_name, daten):
-  with open(file_name, "w") as open_file:
-    json.dump(daten,open_file)
+    with open(file_name, "w") as open_file:
+        json.dump(daten, open_file)
+
 
 def read_data_from_json(file_name):
-  try: 
-    with open(file_name, "r") as open_file:
-      mein_eingelesenes_dict = json.load(open_file)
-      return mein_eingelesenes_dict
-  except Exception:
-    return []   
+    try:
+        with open(file_name, "r") as open_file:
+            mein_eingelesenes_dict = json.load(open_file)
+            return mein_eingelesenes_dict
+    except Exception:
+        return []
 
-#Hier soll das Resultat von result.html geladen werden
-@app.route('/result',methods = ['POST', 'GET'])
+
+# Hier soll das Resultat von result.html geladen werden
+@app.route('/result', methods=['POST', 'GET'])
 def result():
-   if request.method == 'POST':
-      result = request.form
-      print(result)
-      alle_noten = read_data_from_json("notenrechner.json")
-      name = result['Name']
-      vorname = result['Vorname']
-      semester = result['Semester']
-      fach = result['Fach']
-      note = result['Note']
-      neue_noten = {
-        "name": name,
-        "vorname": vorname,
-        "semester": semester,
-        "fach": fach,
-        "note": note,
-      }
+    if request.method == 'POST':
+        formulardaten = request.form
+        print(formulardaten)
+        alle_noten = read_data_from_json("notenrechner.json")
+        name = formulardaten['Name']
+        vorname = formulardaten['Vorname']
+        semester = formulardaten['Semester']
+        fach = formulardaten['Fach']
+        note = formulardaten['Note']
+        neue_noten = {
+            "name": name,
+            "vorname": vorname,
+            "semester": semester,
+            "fach": fach,
+            "note": note
+        }
 
-      alle_noten.append(neue_noten)
-      write_data_to_json("notenrechner.json", alle_noten)
+        alle_noten.append(neue_noten)
+        write_data_to_json("notenrechner.json", alle_noten)
 
-      return render_template("result.html",result = result)
+        return render_template("result.html", result=formulardaten)
+    return render_template('student.html')
 
-#Hier soll das Resultat von durchschnitt.html geladen werden
-@app.route('/durchschnitt',methods = ['POST', 'GET'])
+
+#Hier soll der Durchschnitt von einem Studenten für ein Semester berechnet.
+@app.route('/durchschnitt', methods=['POST', 'GET'])
 def durchschnitt():
-   if request.method == 'POST':
-      durchschnitt = request.form
-      print(durchschnitt)
-      gesamte_auswahl = read_data_from_json("notenrechner.json")
-      name = durchschnitt['Name']
-      vorname = durchschnitt['Vorname']
-      semester = durchschnitt['Semester']
-      neue_auswahl = {
-        "name": name,
-        "vorname": vorname,
-        "semester": semester
-      }
+    if request.method == 'POST':
+        durchschnitt = request.form
+        print(durchschnitt)
+        gesamte_auswahl = read_data_from_json("notenrechner.json")
+        name = durchschnitt['Name']
+        vorname = durchschnitt['Vorname']
+        semester = durchschnitt['Semester']
 
-      return render_template("durchschnitt.html",result = neue_auswahl)  
+        summe_noten = 0
+        anzahl_noten = 0
+        for eintrag in gesamte_auswahl:
+            if eintrag["name"] == name and eintrag["vorname"] == vorname and eintrag["semester"] == semester:
+                summe_noten += float(eintrag["note"])
+                anzahl_noten += 1
+                semesterschnitt = summe_noten/anzahl_noten
+                neue_auswahl = {
+                "Name": name,
+                "Vorname": vorname,
+                "Semester": semester,
+                "Schnitt": semesterschnitt
+                }
 
-  #Hier muss irgendwie das Dict. der Noten eingefügt werden, wie?    
-  #Hier muss irgendwie das oben erstellte dict. neue_auswahl eingefügt werden, wie?
-  #summe_noten = 0
-  #anzahl_noten = 0
-  #for eintrag in noten:
-      #if eintrag["name"] == name and eintrag["vorname"] == vorname and eintrag["semester"] == semester:
-          #summe_noten += float(eintrag["note"])
-          #anzahl_noten += 1    
+        return render_template("durchschnitt.html", data=neue_auswahl)
+    return render_template('auswahl.html')
+
+#Hier soll der Durchschnitt von einem Studenten für das gesamte Studium berechnet.
+@app.route('/gesamtschnitt', methods=['POST', 'GET'])
+def gesamtschnitt():
+    if request.method == 'POST':
+        gesamtschnitt = request.form
+        print(gesamtschnitt)
+        gesamte_auswahl = read_data_from_json("notenrechner.json")
+        name = gesamtschnitt['Name']
+        vorname = gesamtschnitt['Vorname']
+
+        summe_aller_noten = 0
+        anzahl_aller_noten = 0
+        for eintrag in gesamte_auswahl:
+            if eintrag["name"] == name and eintrag["vorname"] == vorname:
+                summe_aller_noten += float(eintrag["note"])
+                anzahl_aller_noten += 1
+                studiumsschnitt = summe_aller_noten/anzahl_aller_noten
+                gesamter_schnitt = {
+                "Name": name,
+                "Vorname": vorname,
+                "Schnitt des gesamten Studiums": studiumsschnitt
+                }
+
+        return render_template("gesamtschnitt.html", gesamt=gesamter_schnitt)
+    return render_template('auswahl2.html')
+ 
+ # Hier sollen alle bereits erfassten Noten eines Studenten dargestellt werden.
+@app.route('/dashboard', methods=['POST', 'GET'])
+def dashboard():
+    if request.method == 'POST':
+        uebersicht = request.form
+        print(uebersicht)
+        uebersicht_voneinem = read_data_from_json("notenrechner.json")
+        name = uebersicht['Name']
+        vorname = uebersicht['Vorname']
+
+        for eintrag in uebersicht_voneinem:
+            if eintrag["name"] == name and eintrag["vorname"] == vorname:
+                alle_dicts = {
+                "name": name,
+                "vorname": vorname,
+                "semester": semester,
+                "fach": fach,
+                "note": note
+                }
+
+        return render_template("dashboard.html", alle_voneinem=alle_dicts)
+    return render_template("auswahl3.html")
 
 if __name__ == '__main__':
-   app.run(debug = True, port=5000)
+    app.run(debug = True, port=5000)
